1c8a96ff4ad6c09a68e16ae3d5bf6e6440553737
Firkax <157710697+Firkax69@users.noreply.github.com>
2024-06-04 12:25:59 +0100

    feat: add rest of methods

--- linkedList.js
+++ linkedList.js
@@ -40,4 +40,58 @@ export default class LinkedList {
         while (tmp.nextNode != null) tmp = tmp.nextNode;
         return tmp;
     }
+
+    at(index) {
+        let tmp = this.listHead;
+        for (let i = 0; i < index; i++) {
+            tmp = tmp.nextNode;
+            if (tmp == null) return 'There is no item for this index';
+        }
+        return tmp;
+    }
+
+    pop() {
+        let cur = this.listHead;
+        let prev = null;
+        while (cur.nextNode != null) {
+            prev = cur;
+            cur = cur.nextNode;
+        }
+        prev.nextNode = null;
+    }
+
+    contains(value) {
+        let tmp = this.listHead;
+        while (tmp != null) {
+            if (tmp.value === value) return true;
+            tmp = tmp.nextNode;
+        }
+        return false;
+    }
+
+
+    
+    find(value) {
+        let tmp = this.listHead;
+        let index = 0;
+        while (tmp != null) {
+            if (tmp.value === value) return index;
+            tmp = tmp.nextNode;
+            index++;
+        }
+        return null;
+    }
+
+
+
+    tostring() {
+        let tmp = this.listHead;
+        let stringList = '';
+        while (tmp != null) {
+            stringList += `(${tmp.value}) -> `;
+            tmp = tmp.nextNode;
+        }
+        return (stringList += 'null');
+    }
+
 }
